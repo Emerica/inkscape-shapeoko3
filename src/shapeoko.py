03c6a48f5075c99528e2499c42f8f578232a1b0a
MrBoogs <blendz@shaw.ca>
2015-08-17 01:13:16 -0600

    Shapeoko3 Drawing Changes

--- src/shapeoko.py
+++ src/shapeoko.py
@@ -20,28 +20,21 @@ import sys,os
 import inkex
 from math import *
 import getopt
-from unicorn.context import GCodeContext
-from unicorn.svg_parser import SvgParser
+from shapeoko.context import GCodeContext
+from shapeoko.svg_parser import SvgParser
 
 class MyEffect(inkex.Effect):
   def __init__(self):
     inkex.Effect.__init__(self)
-    self.OptionParser.add_option("--pen-up-angle",
-                      action="store", type="float",
-                      dest="pen_up_angle", default="50.0",
-                      help="Pen Up Angle")
-    self.OptionParser.add_option("--pen-down-angle",
-                      action="store", type="float",
-                      dest="pen_down_angle", default="30.0",
-                      help="Pen Down Angle")
+
     self.OptionParser.add_option("--start-delay",
                       action="store", type="float",
-                      dest="start_delay", default="150.0",
-                      help="Delay after pen down command before movement in milliseconds")
+                      dest="start_delay", default="0.1",
+                      help="Delay after pen down command before movement in s")
     self.OptionParser.add_option("--stop-delay",
                       action="store", type="float",
-                      dest="stop_delay", default="150.0",
-                      help="Delay after pen up command before movement in milliseconds")
+                      dest="stop_delay", default="0.1",
+                      help="Delay after pen up command before movement in s")
     self.OptionParser.add_option("--xy-feedrate",
                       action="store", type="float",
                       dest="xy_feedrate", default="3500.0",
@@ -56,12 +49,8 @@ class MyEffect(inkex.Effect):
                       help="Z axis print height in mm")
     self.OptionParser.add_option("--finished-height",
                       action="store", type="float",
-                      dest="finished_height", default="0.0",
+                      dest="finished_height", default="5.0",
                       help="Z axis height after printing in mm")
-    self.OptionParser.add_option("--register-pen",
-                      action="store", type="string",
-                      dest="register_pen", default="true",
-                      help="Add pen registration check(s)")
     self.OptionParser.add_option("--x-home",
                       action="store", type="float",
                       dest="x_home", default="0.0",
@@ -70,13 +59,6 @@ class MyEffect(inkex.Effect):
                       action="store", type="float",
                       dest="y_home", default="0.0",
                       help="Starting Y position")
-    self.OptionParser.add_option("--num-copies",
-                      action="store", type="int",
-                      dest="num_copies", default="1")
-    self.OptionParser.add_option("--continuous",
-                      action="store", type="string",
-                      dest="continuous", default="false",
-                      help="Plot continuously until stopped.")
     self.OptionParser.add_option("--pause-on-layer-change",
                       action="store", type="string",
                       dest="pause_on_layer_change", default="false",
@@ -91,12 +73,8 @@ class MyEffect(inkex.Effect):
   def effect(self):
     self.context = GCodeContext(self.options.xy_feedrate, self.options.z_feedrate, 
                            self.options.start_delay, self.options.stop_delay,
-                           self.options.pen_up_angle, self.options.pen_down_angle,
                            self.options.z_height, self.options.finished_height,
                            self.options.x_home, self.options.y_home,
-                           self.options.register_pen,
-                           self.options.num_copies,
-                           self.options.continuous,
                            self.svg_file)
     parser = SvgParser(self.document.getroot(), self.options.pause_on_layer_change)
     parser.parse()
---
+++ src/shapeoko/context.py
@@ -0,0 +1,129 @@
+from math import *
+import sys
+
+class GCodeContext:
+    def __init__(self, xy_feedrate, z_feedrate, start_delay, stop_delay, z_height, finished_height, x_home, y_home, file):
+      self.xy_feedrate = xy_feedrate
+      self.z_feedrate = z_feedrate
+      self.start_delay = start_delay
+      self.stop_delay = stop_delay
+      self.z_height = z_height
+      self.finished_height = finished_height
+      self.x_home = x_home
+      self.y_home = y_home
+      self.file = file
+      self.drawing = False
+      self.last = None
+
+      self.preamble = [
+        #"(Scribbled version of %s @ %.2f)" % (self.file, self.xy_feedrate),
+        #"( %s )" % " ".join(sys.argv),
+        "G21 (metric ftw)",
+        "G90 (absolute mode)",
+        #"G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+        ""
+      ]
+
+      self.postscript = [
+        "",
+        #"(end of print job)",
+        "G0 Z%d (pen up)" % self.finished_height,
+        "G4 P%d (wait %ds)" % (self.stop_delay, self.stop_delay),
+        #"M300 S255 (turn off servo)",
+        "G1 X0 Y0 F%0.2F" % self.xy_feedrate,
+        #"G1 Z%0.2F F%0.2F (go up to finished level)" % (self.finished_height, self.z_feedrate),
+        #"G1 X%0.2F Y%0.2F F%0.2F (go home)" % (self.x_home, self.y_home, self.xy_feedrate),
+        #"M18 (drives off)",
+      ]
+
+      self.registration = [
+        #"G0 Z%d (pen down)" % self.z_height,
+        #"G4 P%d (wait %ds)" % (self.start_delay, self.start_delay),
+        #"G0 Z%d (pen up)" % self.finished_height,
+        #"G4 P%d (wait %ds)" % (self.stop_delay, self.stop_delay),
+        #"M18 (disengage drives)",
+        #"M01 (Was registration test successful?)",
+        #"M17 (engage drives if YES, and continue)",
+        ""
+      ]
+
+      self.sheet_header = [
+        "(start of sheet header)",
+        #"G92 X%.2f Y%.2f Z%.2f (you are here)" % (self.x_home, self.y_home, self.z_height),
+      ]
+      
+      #if self.register_pen == 'true':
+        #self.sheet_header.extend(self.registration)
+      #self.sheet_header.append("(end of sheet header)")
+
+      self.sheet_footer = [
+        "(Start of sheet footer.)",
+        "G0 Z%d (pen up)" % self.finished_height,
+        "G4 P%d (wait %ds)" % (self.stop_delay, self.stop_delay),
+        "G91 (relative mode)",
+        "G0 Z15 F%0.2f" % (self.z_feedrate),
+        "G90 (absolute mode)",
+        "G0 X%0.2f Y%0.2f F%0.2f" % (self.x_home, self.y_home, self.xy_feedrate),
+        "M01 (Have you retrieved the print?)",
+        "(machine halts until 'okay')",
+        "G4 P%d (wait %ds)" % (self.start_delay, self.start_delay),
+        "G91 (relative mode)",
+        "G0 Z-15 F%0.2f (return to start position of current sheet)" % (self.z_feedrate),
+        "G0 Z-0.01 F%0.2f (move down one sheet)" % (self.z_feedrate),
+        "G90 (absolute mode)",
+        #"M18 (disengage drives)",
+        "(End of sheet footer)",
+      ]
+
+
+      self.codes = []
+
+    def generate(self):
+      #if self.continuous == 'true':
+      self.num_pages = 1
+
+      codesets = [self.preamble]
+      codesets.append(self.codes)
+
+      #for p in range(0,self.num_pages):
+      for codeset in codesets:
+        for line in codeset:
+          print line
+        for line in self.postscript:
+          print line
+
+    def start(self):
+      self.codes.append("G0 Z%d (pen down)" % self.z_height)
+      self.codes.append("G4 P%d (wait %ds)" % (self.start_delay, self.start_delay))
+      self.drawing = True
+
+    def stop(self):
+      self.codes.append("G0 Z%d (pen up)" % self.finished_height)
+      self.codes.append("G4 P%d (wait %ds)" % (self.stop_delay, self.stop_delay))
+      self.drawing = False
+
+    def go_to_point(self, x, y, stop=False):
+      if self.last == (x,y):
+        return
+      if stop:
+        return
+      else:
+        if self.drawing: 
+            self.codes.append("G0 Z%d (pen up)" % self.finished_height)
+            self.codes.append("G4 P%d (wait %ds)" % (self.stop_delay, self.stop_delay))
+            self.drawing = False
+        self.codes.append("G1 X%.2f Y%.2f F%.2f" % (x,y, self.xy_feedrate))
+      self.last = (x,y)
+
+    def draw_to_point(self, x, y, stop=False):
+      if self.last == (x,y):
+          return
+      if stop:
+        return
+      else:
+        if self.drawing == False:
+            self.codes.append("G0 Z%d (pen down)" % self.z_height)
+            self.codes.append("G4 P%d (wait %ds)" % (self.start_delay, self.start_delay))
+            self.drawing = True
+        self.codes.append("G1 X%0.2f Y%0.2f F%0.2f" % (x,y, self.xy_feedrate))
+      self.last = (x,y)
